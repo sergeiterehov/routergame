5d4e94faf8cc3e0f6fc8df387c31fd2d8e21c11f
Sergei Terekhov <isergeiterehov@gmail.com>
2026-05-24 12:07:47 +0300

    Исправлена сборка

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+  base: process.env.VITE_BASE_URL,
 });
